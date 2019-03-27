5371d85b81e2f84393b2c9a643ef26db1e69ea40
ma-za-kpe <makpalyy@gmail.com>
2019-03-27 17:38:31 +0200

    add a food tracker form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMealComponent } from './add-meal/add-meal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMealComponent
   ],
   imports: [
     BrowserModule,
